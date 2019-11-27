b054e6e901a48017bd256e850b4f329e8fdc1b29
Handong Kim <aggsae@gmail.com>
2019-11-27 19:29:24 +0900

    눈 건강 정보 확인 디자인 수정
    기존 activity_tip 삭제
    직관적인 이름의 activity_tip_select로 변경

--- Black/app/src/main/java/com/example/black/EyeTipSelectActivity.kt
+++ Black/app/src/main/java/com/example/black/EyeTipSelectActivity.kt
@@ -4,6 +4,7 @@
  * 눈 관련 정보들을 확인할 수 있는 버튼이 있는 activity
  * history
  * 20191122     handongkim      init
+ * 20191127     handongkim      xml 디자인 수정
  */
 
 package com.example.black
@@ -11,42 +12,42 @@ package com.example.black
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_tips.*
+import kotlinx.android.synthetic.main.activity_tip_select.*
 
 class EyeTipSelectActivity  : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tips)
+        setContentView(R.layout.activity_tip_select)
 
-        val selectBtnListener = btnListener()
+        val selectBtnListener = selectBtnListener()
 
-        button2.setOnClickListener(selectBtnListener)
-        button3.setOnClickListener(selectBtnListener)
-        button4.setOnClickListener(selectBtnListener)
-        button5.setOnClickListener(selectBtnListener)
-        button6.setOnClickListener(selectBtnListener)
+        foodBtn.setOnClickListener(selectBtnListener)
+        teaBtn.setOnClickListener(selectBtnListener)
+        drugBtn.setOnClickListener(selectBtnListener)
+        excerciseBtn.setOnClickListener(selectBtnListener)
+        infoBtn.setOnClickListener(selectBtnListener)
 
     } // end of onCreate
 
     private fun sendIntent(args:String) {
-        Toast.makeText(this, args + "로 이동", Toast.LENGTH_SHORT).show()
+        // Toast.makeText(this, args + "로 이동", Toast.LENGTH_SHORT).show()
 
         val intent = Intent(this, EyeTipActivity::class.java)
+        // TODO :: DB 연결의 시작, 각기 다른 데이터베이스를 가져와서 내용을 보여줘야 한다.
         intent.putExtra("key", args)
         startActivity(intent)
     }
 
-    inner class btnListener : View.OnClickListener {
+    inner class selectBtnListener : View.OnClickListener {
         override fun onClick(v: View?) {
             when(v?.id) {
-                R.id.button2 -> sendIntent(button2.text.toString())
-                R.id.button3 -> sendIntent(button3.text.toString())
-                R.id.button4 -> sendIntent(button4.text.toString())
-                R.id.button5 -> sendIntent(button5.text.toString())
-                R.id.button6 -> sendIntent(button6.text.toString())
+                R.id.foodBtn -> sendIntent(foodBtn.text.toString())
+                R.id.teaBtn -> sendIntent(teaBtn.text.toString())
+                R.id.drugBtn -> sendIntent(drugBtn.text.toString())
+                R.id.excerciseBtn -> sendIntent(excerciseBtn.text.toString())
+                R.id.infoBtn -> sendIntent(infoBtn.text.toString())
             }
         }
     }
