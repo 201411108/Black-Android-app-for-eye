76259ec4379f76fbae651c8c4234dc0b2138c514
KyeongMoon <52391290+KyeongMoon@users.noreply.github.com>
2019-11-30 15:17:12 +0900

    MainActivity2.kt .xml 삭제

--- Black/app/src/main/java/com/example/black/MainActivity.kt
+++ Black/app/src/main/java/com/example/black/MainActivity.kt
@@ -11,6 +11,7 @@ import android.os.Handler
 import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.WindowManager
+import android.widget.SeekBar
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat.getSystemService
 import androidx.core.graphics.toColor
@@ -30,55 +31,78 @@ class MainActivity : AppCompatActivity() {
 
         //어플리케이션 실행 시 서비스가 실행중인지 확인, 실행중이면 이미지 크기를 큰것으로 교체
         if (checkState()) {
-            kkmTestImage.setBackgroundResource(R.drawable.ic_launcher_foreground)
+            turnOnBtn.setImageResource(R.drawable.end_button2)
             isRunning = true
         }
 
-        //권한 확인, 권한을 이미 얻었다면 권한 추가 창을 생성하지 않음.
-        overlay_permission.setOnClickListener {
-            if (!Settings.canDrawOverlays(this)) {
-                startActivity(
-                    Intent(
-                        "android.settings.action.MANAGE_OVERLAY_PERMISSION",
-                        Uri.parse("package:" + getPackageName())
-                    )
-                )
+        turnOnBtn.setOnClickListener {
+
+
+            //Overlay 권한 확인
+            if (PermissionCheck()) {
+                //중지 -> 실행
+                if(!isRunning) {
+                    // Toast.makeText(this, "서비스 실행되며 종료 버튼으로 전환 " + onOffChecker, Toast.LENGTH_SHORT).show()
+
+                    // 모든 값이 입력되어야 서비스 실행
+                    // 입력하지 않을 경우 토스트 메세지 발생
+//                isRunning = !startServiceWhenNotNull(!isRunning, periodText2.text.toString(), sustainTimeText2.text.toString(), colorText2.text.toString())
+                    //isRunning = !startServiceWhenNotNull(!isRunning, periodSeekBar.progress.toString(), sustainSeekBar.progress.toString(), colorSeekBar.progress.toString())
+
+                    val intent = Intent(this, NormalService::class.java)
+                    intent.putExtra("inputPeriod", periodSeekBar.progress.toString().toLong()*1000)
+                    intent.putExtra("inputSustainTime", sustainTimeSeekBar.progress.toString().toLong())
+                    intent.putExtra("inputColor", colorSeekBar.progress.toString().toInt()*250/100)
+                    startService(intent)
+                    // 서비스 시작 후 이미지 변경(안드로이가 서비스 실행 중 -> 중지 버튼)
+                    turnOnBtn.setImageResource(R.drawable.end_button2)
+
+
+                    isRunning = true
+                }
+
+                //실행 -> 중지
+                else {
+                    // Toast.makeText(this, "서비스 종료하며 시작 버튼으로 전환 " + onOffChecker, Toast.LENGTH_SHORT).show()
+                    // 구글이 서비스 실행 안되고 있는 중 -> 시작 버튼
+                    turnOnBtn.setImageResource(R.drawable.start_button)
+
+                    // 서비스 중지
+                    stopService(Intent(this, NormalService::class.java))
+                    isRunning = false
+                }
             }
         }
 
-        //서비스 시작
-        service_init.setOnClickListener {
-            var intent = Intent(this, NormalService::class.java)
-            intent.putExtra("inputPeriod", periodText.text.toString().toLong())
-            intent.putExtra("inputSustainTime", sustainTimeText.text.toString().toLong())
-            intent.putExtra("inputColor", colorText.text.toString().toInt())
-
-
-            if (!isRunning) {
-                kkmTestImage.setBackgroundResource(R.drawable.ic_launcher_foreground)
-                startService(intent)
-                isRunning = true
-            }
-        }
-
-        //서비스 중지
-        service_stop.setOnClickListener {
-            if (isRunning) {
-                stopService(Intent(this, NormalService::class.java))
-                isRunning = false
-            }
-        }
-
-        button.setOnClickListener {
-            val intent = Intent(this, MainActivity2::class.java)
+        tipBtn.setOnClickListener {
+            val intent = Intent(this, EyeTipSelectActivity::class.java)
             startActivity(intent)
         }
 
-        //임시로 만든 TEST ACTIVITY
-        isgbutton.setOnClickListener {
+        isgBtn.setOnClickListener {
             val intent = Intent(this, ISGTestActivity::class.java)
             startActivity(intent)
         }
+
+        //TODO("seekbar 3개")
+        periodSeekBar.setOnSeekBarChangeListener(PeriodSeekBarListener())
+        sustainTimeSeekBar.setOnSeekBarChangeListener(SustainTimeSeekBarListener())
+        colorSeekBar.setOnSeekBarChangeListener(ColorSeekBarListener())
+
+    } // end of onCreate
+
+
+    fun PermissionCheck() : Boolean {
+        if (!Settings.canDrawOverlays(this)) {
+            startActivity(
+                Intent(
+                    "android.settings.action.MANAGE_OVERLAY_PERMISSION",
+                    Uri.parse("package:" + getPackageName())
+                )
+            )
+            return false
+        }
+        return true
     }
 
     //서비스가 이미 실행중인지 확인하는 함수
@@ -89,5 +113,50 @@ class MainActivity : AppCompatActivity() {
             }
         }
         return false
+    }
+
+    inner class PeriodSeekBarListener : SeekBar.OnSeekBarChangeListener{
+
+        override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+            periodSeekBar.progress = progress
+            periodSeekBarText.text = "${seekBar?.progress.toString()}초"
+        }
+
+        override fun onStartTrackingTouch(seekBar: SeekBar?) {
+        }
+
+        override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            periodSeekBarText.text = "주기 ${seekBar?.progress.toString()}초"
+        }
+    }
+
+    inner class SustainTimeSeekBarListener : SeekBar.OnSeekBarChangeListener{
+
+        override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+            seekBar?.progress = (progress/10)*10
+            sustainTimeSeekBarText.text = "${seekBar?.progress.toString()}ms"
+        }
+
+        override fun onStartTrackingTouch(seekBar: SeekBar?) {
+        }
+
+        override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            sustainTimeSeekBarText.text = "지속시간 ${seekBar?.progress.toString()}ms"
+        }
+    }
+
+    inner class ColorSeekBarListener : SeekBar.OnSeekBarChangeListener{
+
+        override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+            seekBar?.progress = (progress/10)*10
+            colorSeekBarText.text = "${seekBar?.progress.toString()}%"
+        }
+
+        override fun onStartTrackingTouch(seekBar: SeekBar?) {
+        }
+
+        override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            colorSeekBarText.text = "투명도 ${seekBar?.progress.toString()}%"
+        }
     }
 }
